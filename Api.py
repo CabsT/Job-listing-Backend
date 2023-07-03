8cebb7df9ff1e5d449f51929fb0ee16185fd2dd9
Charne Teichert <charne.teichert@gmail.com>
2023-07-03 10:07:27 +0200

    Route and function for HomePage to render the 'Job Form' -> In progress

--- Api.py
+++ Api.py
@@ -5,3 +5,15 @@ from flask_cors import CORS
 # Uing __name__ as the argument to use the current module as the name of the application package.
 app = Flask(__name__) 
 CORS(app)  # Allow Cross-Origin Resource Sharing
+
+##HomePage -> In Progress
+@app.route('/')
+def main():
+    return "Job Listing HomePage"
+
+#Route and function to render the 'Job Form' -> In progress
+@app.route('/joblist/add')
+def add_job():
+    return "Job Added/Job Form"
+
+
