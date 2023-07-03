a781e41115e472b2265b18344d6210fb67bb636a
Charne Teichert <charne.teichert@gmail.com>
2023-07-03 09:48:07 +0200

    Added comments to Api file

--- Api.py
+++ Api.py
@@ -1,2 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+
+#The 'app' object is an instance of the Flask class
+# Uing __name__ as the argument to use the current module as the name of the application package.
+app = Flask(__name__) 
+CORS(app)  # Allow Cross-Origin Resource Sharing
