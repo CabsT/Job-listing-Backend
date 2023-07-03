93730fa5c4613caa7f4b00b68a1277d57fac0470
Charne Teichert <charne.teichert@gmail.com>
2023-07-03 11:04:41 +0200

    Import os and psycopg2 module to manage connection with database amd interact with operating system.

---
+++ db.py
@@ -0,0 +1,4 @@
+#The os module in Python provides a way to interact with the operating system.
+import os
+#The psycopg2 module provides a pool submodule that allows you to create and manage a connection pool for PostgreSQL database.
+from psycopg2 import pool
