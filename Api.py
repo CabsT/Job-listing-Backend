b135821745dc766f799efb306d3972bf7a19c971
Charne Teichert <charne.teichert@gmail.com>
2023-07-03 16:16:10 +0200

    Import the db class from the db module

--- Api.py
+++ Api.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+#Import the db class from the db.py file 
+from db import db
 
 #The 'app' object is an instance of the Flask class
 # Uing __name__ as the argument to use the current module as the name of the application package.
