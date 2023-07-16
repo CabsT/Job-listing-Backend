87d84a932eb455a163fff67169ee17bc566c3641
Charne Teichert <charne.teichert@gmail.com>
2023-07-16 16:10:07 +0200

    Updated the "job_application' table names in the dict to match the names in my updated sql_statements file.

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,8 +1,8 @@
 CREATE TABLE job_applications (
   id SERIAL PRIMARY KEY,
   jobId INTEGER,
-  full_name VARCHAR(255) ,
+  fullName VARCHAR(255) ,
   email VARCHAR(255),
-  cover_letter VARCHAR(3000),
-  cv_file BYTEA
+  coverLetter VARCHAR(3000),
+  cvFile BYTEA
 );
